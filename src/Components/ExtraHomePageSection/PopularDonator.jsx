e008fb9c3ff36de70b303a1d39943e611baea62f
Erazul Islam <erazul89@gmail.com>
2023-11-08 01:15:15 +0600

    slider done

--- src/Components/ExtraHomePageSection/PopularDonator.jsx
+++ src/Components/ExtraHomePageSection/PopularDonator.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Marquee from "react-fast-marquee";
 
 const PopularDonator = () => {
 
@@ -17,25 +18,27 @@ const PopularDonator = () => {
             <div>
                 <p className="text-4xl text-orange-500 font-bold text-center">Our most efficient donator</p>
             </div>
-            <div className="mt-10 grid gap-8 grid-cols-3 lg:ml-[200px]">
-                {
-                    donators.map(donator => <div key={donator._id} className="card w-96 bg-base-100 shadow-xl">
-                        <figure className="px-10 pt-10">
-                            <img src={donator.donator_img} alt="Shoes" className="rounded-xl" />
-                        </figure>
-                        <div className="card-body items-center text-center">
-                            <h2 className="card-title">{donator.donator_name}</h2>
-                            <div className="">
-                                <Link className="flex gap-4">
-                                    <img className="w-8" src="https://i.ibb.co/7pSPmmb/5296500-fb-social-media-facebook-facebook-logo-social-network-icon.png" alt="" />
-                                    <img className="w-8" src="https://i.ibb.co/fMtHfLz/3225191-app-instagram-logo-media-popular-icon.png" alt="" />
-                                    <img className="w-8" src="https://i.ibb.co/F4xWrKH/5296514-bird-tweet-twitter-twitter-logo-icon.png" alt="" />
-                                </Link>
+            <Marquee pauseOnHover={true} >
+                <div className="mt-10 grid gap-8 grid-cols-3 lg:ml-[200px]">
+                    {
+                        donators.map(donator => <div key={donator._id} className="card w-96 bg-base-100 shadow-xl">
+                            <figure className="px-10 pt-10">
+                                <img src={donator.donator_img} alt="Shoes" className="rounded-xl" />
+                            </figure>
+                            <div className="card-body items-center text-center">
+                                <h2 className="card-title">{donator.donator_name}</h2>
+                                <div className="">
+                                    <Link className="flex gap-4">
+                                        <img className="w-8" src="https://i.ibb.co/7pSPmmb/5296500-fb-social-media-facebook-facebook-logo-social-network-icon.png" alt="" />
+                                        <img className="w-8" src="https://i.ibb.co/fMtHfLz/3225191-app-instagram-logo-media-popular-icon.png" alt="" />
+                                        <img className="w-8" src="https://i.ibb.co/F4xWrKH/5296514-bird-tweet-twitter-twitter-logo-icon.png" alt="" />
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                    </div>)
-                }
-            </div>
+                        </div>)
+                    }
+                </div>
+            </Marquee>
         </div>
     );
 };
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,85 +1,24 @@
-
+import Marquee from "react-fast-marquee";
 
 const Slider = () => {
     return (
         <div>
-            <div className="carousel h-[800px] w-full">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://i.ibb.co/C0ZqrdH/lily-banse-YHSwy6uqvk-unsplash.jpg" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
+            <div className="relative">
+                <img src="https://i.ibb.co/S7JQ9ZW/360-F-355732227-Pne-Uq5-XPTr-D0t-GT7j6-EMg-FZZAy-Su7-Pj-N.jpg" className="w-full lg:h-96" alt="" />
+
+                <div className="absolute bottom-1 ">
+                    <div>
+                        
+                        <div>
+                            <p className="lg:text-6xl text-pink-700 lg:ml-12 font-bold lg:pb-40">Be the Change <br /> You Want to See</p>
+                            <Marquee className="text-3xl pb-5" pauseOnHover={true}>
+                                Help us make the world a better place by supporting our mission to [briefly describe your cause or goal]. Your contribution can change lives.
+                            </Marquee>
+                        </div>
                     </div>
                 </div>
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src=" https://i.ibb.co/FsMJrx7/anh-nguyen-kc-A-c3f-3-FE-unsplash.jpg" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src=" https://i.ibb.co/FsMJrx7/anh-nguyen-kc-A-c3f-3-FE-unsplash.jpg" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide2" className="btn btn-circle">❮</a>
-                        <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
+                
             </div>
-
-
-            {/* <div id="default-carousel" className="relative w-full" data-carousel="slide">
-
-                <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://i.ibb.co/C0ZqrdH/lily-banse-YHSwy6uqvk-unsplash.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://i.ibb.co/FsMJrx7/anh-nguyen-kc-A-c3f-3-FE-unsplash.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src=" https://i.ibb.co/FsMJrx7/anh-nguyen-kc-A-c3f-3-FE-unsplash.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://i.ibb.co/C0ZqrdH/lily-banse-YHSwy6uqvk-unsplash.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-
-                    <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://i.ibb.co/FsMJrx7/anh-nguyen-kc-A-c3f-3-FE-unsplash.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                    </div>
-                </div>
-
-                <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                    <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                </div>
-
-                <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"  strokeWidth="2" d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span className="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"  strokeWidth="2" d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span className="sr-only">Next</span>
-                    </span>
-                </button>
-            </div> */}
-
-
         </div>
     );
 };
